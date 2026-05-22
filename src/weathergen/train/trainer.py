1c6f7686b984d1c662461c4e1ec9ff37eae444ef
Patnala,Ankit <a.patnala@fz-juelich.de>
2026-05-22 17:49:05 +0200

    updated config to facilitate muon optimizer

--- src/weathergen/train/trainer.py
+++ src/weathergen/train/trainer.py
@@ -704,7 +704,7 @@ class Trainer(TrainerBase):
     def _apply_muon_lr_scale(self):
         """After lr_scheduler updates all param groups, re-scale the Muon group's lr."""
         if self._use_muon and self._muon_lr_scale != 1.0:
-            self.optimizer.param_groups[0]["lr"] *= self._muon_lr_scale
+            self.optimizer.param_groups[0]["lr"] = self.lr_scheduler.get_lr() * self._muon_lr_scale
 
     def _get_full_optimizer_state_dict(self):
         is_rank_zero = is_root()
